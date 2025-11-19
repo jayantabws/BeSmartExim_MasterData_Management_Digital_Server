c344f7c63576aef47105a2530365ed52106f7b5d
Pabitra Ghosal <pabitraghosal5@gmail.com>
2025-11-19 15:20:34 +0530

    getdate API for show date.
    getdate API for show date.

--- src/main/java/com/besmartexim/controller/MasterdataController.java
+++ src/main/java/com/besmartexim/controller/MasterdataController.java
@@ -1,5 +1,29 @@
 package com.besmartexim.controller;
 
-public class MasterdataController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
+@RequestMapping(path = "/masterdata-management")
+public class MasterdataController {
+	private static final Logger logger = LoggerFactory.getLogger(MasterdataController.class);
+
+	@Value("${app.date}")
+    private String appDate;
+	
+	
+	@GetMapping("/getdate")
+	public ResponseEntity<String> getPropertyValue(@RequestHeader(value = "accessedBy", required = true) Long accessedBy) {
+		logger.info("Request : /masterdata-management/getdate");
+		return new ResponseEntity<>(appDate, HttpStatus.OK);
+    }
 }
