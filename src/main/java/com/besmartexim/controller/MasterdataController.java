1f04c9b7122f013785a1517fafe2a5fdd0e36aac
Pabitra Ghosal <pabitraghosal5@gmail.com>
2026-01-29 10:57:05 +0530

    removed /getdate API.
    removed /getdate API.

--- src/main/java/com/besmartexim/controller/MasterdataController.java
+++ src/main/java/com/besmartexim/controller/MasterdataController.java
@@ -1,12 +1,6 @@
 package com.besmartexim.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/masterdata-management")
 public class MasterdataController {
-	private static final Logger logger = LoggerFactory.getLogger(MasterdataController.class);
+	
 
-	@Value("${app.date}")
-    private String appDate;
-	
-	
-	@GetMapping("/getdate")
-	public ResponseEntity<String> getPropertyValue() {
-		logger.info("Request : /masterdata-management/getdate");
-		return new ResponseEntity<>(appDate, HttpStatus.OK);
-    }
 }
